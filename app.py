969abb1a7138aa8eb0a5f848e14014ccd2454a43
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-12-20 16:20:31 +0200

    website send twist

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import rospy as rp
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 from time import sleep
 from flask import Flask, render_template, request
 
@@ -8,26 +8,40 @@ from flask import Flask, render_template, request
 rp.init_node("serv_pub")
 rp.loginfo("hello from serv_pub")
 # Create publisher
-pub = rp.Publisher("/torta_cmd", String, queue_size=10)
+pub = rp.Publisher("/cmd_vel", Twist, queue_size=10)
 
 
 # Initialize flask
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
-    max_linear = 0
-    max_rotation = 0
     
     if request.method == "POST":
-        max_linear = request.form.get("max_linear")
-        max_rotation = request.form.get("max_angle")
+        x = request.form.get("x")
+        y = request.form.get("y")
+        theta = request.form.get("theta")
+        try:
+            x = int(x)
+        except:
+            x = 0
+
+        try:
+            y = int(y)
+        except:
+            y = 0
+        try:
+            theta = int(theta)
+        except:
+            theta = 0
+
+        my_msg = Twist()
         # Publish recieved values
-        if max_linear == None and max_rotation == None:
-            publish_str = "STOP"
-        else:
-            publish_str = "linear is " + str(max_linear) + " and angle is " + str(max_rotation)
-        pub.publish(publish_str)
-        rp.loginfo(publish_str)
+        my_msg.linear.x = x
+        my_msg.linear.y = y
+        my_msg.angular.z = theta
+        msg_str = f"x = {my_msg.linear.x}, y = {my_msg.linear.y}, theta = {my_msg.angular.z}"
+        pub.publish(my_msg)
+        rp.loginfo(msg_str)
     return render_template("index.html")
 
     
